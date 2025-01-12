7f214f657541da90eb98555e9c240d380862d818
G4EVA-dev <tanzeglenn@gmail.com>
2025-01-12 22:42:25 +0100

    complete navbar and hero

--- app/components/common/hero.tsx
+++ app/components/common/hero.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold ">Welcome To My 3D Portfolio</h1>
+    <div className="mt-[450px]  ">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold ">Welcome To My 3D Portfolio</h1>
+        <p className="mt-7 text-2xl">
+          I&apos;m a developer passionate about creating interactive and
+          visually appealing web experiences.
+        </p>
+      </div>
+      <div className="flex justify-center items-center gap-10 mt-20">
+        <button className="border-[1px] border-gray-300 bg-black text-white py-2 px-4 rounded-md hover:opacity-50 ">
+          Get Your Resume
+        </button>
+        <button className="border-[1px] border-gray-300 bg-white text-black py-2 px-4 rounded-md hover:opacity-50 ">
+          View My Projects
+        </button>
+      </div>
     </div>
   );
 }
--- app/components/common/nav.tsx
+++ app/components/common/nav.tsx
@@ -2,28 +2,32 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="w-full flex justify-around items-center h-27 py-10 bg-slate-50 shadow-lg ">
-      <div>Resume Builder</div>
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-90 backdrop-blur-sm shadow-sm ">
+      <div className="flex justify-between items-center p-6  md:space-x-10">
+        <div className="text-3xl font-bold">Resume Builder</div>
 
-      <div>
-        <ul className="flex justify-around items-center gap-10">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Resume</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
+        <div>
+          <ul className="flex justify-around items-center gap-10">
+            <li className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href="#">Home</a>
+            </li>
+            <li className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href="#">Projects</a>
+            </li>
+            <li className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href="#">Resume</a>
+            </li>
+            <li className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+        </div>
 
-      <div>
-        <button>Download CV</button>
+        <div>
+          <button className="border-[1px] border-gray-400 py-2 px-6 rounded-md ">
+            Download CV
+          </button>
+        </div>
       </div>
     </nav>
   );
