ea7f4802fd876d87aa5d0f83dff234a4b152da0d
G4EVA-dev <tanzeglenn@gmail.com>
2025-01-13 21:49:36 +0100

    better nav but not best

--- app/components/common/nav.tsx
+++ app/components/common/nav.tsx
@@ -87,7 +87,7 @@ export default function NavBar() {
         </ul>
 
         <div className="mt-10 flex justify-center">
-          <button className="border-[1px] border-gray-400 text-white py-2 px-6 rounded-md hover:bg-gray-700">
+          <button className="border-[1px] border-gray-400 text-black py-2 px-6 rounded-md hover:bg-gray-700">
             Join Us
           </button>
         </div>
