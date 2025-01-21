44f2cd52e0529fcfeb8d36a6b94c7a9de9a32618
Glenn Tanze <71010568+G4EVA-dev@users.noreply.github.com>
2025-01-21 22:30:24 +0100

    Update hero.tsx
    update hero

--- app/components/common/hero.tsx
+++ app/components/common/hero.tsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="mt-[550px] mb-[50px] ">
+    <div className="mt-[600px] mb-[50px] ">
       <div className="text-center">
         <h1 className="text-5xl font-bold ">Welcome To ResumeBuilder</h1>
 
         <p className="mt-7 text-2xl slide-up">
-          Craft Your Perfect Resume in Minutes!
+          Craft Your Perfect Resume in Minutes! 😁 
         </p>
       </div>
       <div className="flex justify-center items-center gap-10 mt-20">
         <Link href="/resume">
           <button className="border-[1px] border-gray-300 bg-black text-white py-2 px-4 rounded-md hover:opacity-50 ">
-            Get Your Resume
+            Get Your Resume 
           </button>
         </Link>
 
         <button className="border-[1px] border-gray-300 bg-white text-black py-2 px-4 rounded-md hover:opacity-50 ">
-          View Projects
+          View Projects 🔎 
         </button>
       </div>
     </div>
